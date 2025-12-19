de92dd9542d9e8ec968cd4bcfade2e5d158ae84e
Konstantin <kosmos.94@gmail.com>
2025-12-19 15:26:07 +0200

    Add test2

--- lesson_2/test2.py
+++ lesson_2/test2.py
@@ -1,1 +1,13 @@
-print("test2")
+def prime_nums(num):
+    primes = []
+    for i in range(2, num + 1):
+        for j in range(2, i):
+            if i % j == 0:
+                break
+        else:
+            primes += [i]
+    return(primes)
+
+
+num = int(input("Введи число: "))
+print(prime_nums(num))
